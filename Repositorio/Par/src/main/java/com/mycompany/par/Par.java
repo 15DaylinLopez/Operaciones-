12b2147dfbbf8facf4333ad622248d8344673a67
15DaylinLopez <162522890+15DaylinLopez@users.noreply.github.com>
2024-03-13 11:10:56 -0600

    Add files via upload

---
+++ Repositorio/Par/src/main/java/com/mycompany/par/Par.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.par;
+
+/**
+ *
+ * @author CP07
+ */
+import java.util.Scanner ; 
+public class Par {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner (System.in);
+        System.out.println("Ingrese un numero entero:");
+    int numero = scanner.nextInt ();
+    if (numero % 2 == 0 ){
+        System.out.println ("Este numero es impar.");
+       }else{
+        System.out.println ("Este numero es impar.");
+    }
+    scanner.close(); 
+}
+    }
+
+    
